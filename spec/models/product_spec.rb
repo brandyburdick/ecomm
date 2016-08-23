f651d9a991be3a764b3229e1e09a603e6917cab6
Brandy Burdick <blburdic@asu.edu>
2016-08-23 12:47:27 -0700

    Refactor product_spec code to be more concise

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,48 @@
+#BEFORE REFACTORING
+
+# require 'rails_helper'
+
+# RSpec.describe Product, type: :model do
+#   it "doesn't allow a product's name to be nil" do
+#      macbook = FactoryGirl.build(:product, name: nil)
+#      expect(macbook).to_not be_valid
+#   end
+
+#   it "doesn't allow a product's quantity to be nil" do
+#      macbook = FactoryGirl.build(:product, quantity: nil)
+#      expect(macbook).to_not be_valid
+#   end
+
+#   it "doesn't allow a product's quantity to be negative" do
+#      macbook = FactoryGirl.build(:product, quantity: -1)
+#      expect(macbook).to_not be_valid
+#   end
+
+# end
+
+#AFTER REFACTORING
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it "doesn't allow a product's name to be nil" do
-     macbook = FactoryGirl.build(:product, name: nil)
-     expect(macbook).to_not be_valid
-  end
+	describe "name" do
+	  it "cannot be nil" do
+	    product = FactoryGirl.build(:product, name: nil)
+	  end
+	end
+	  
+	describe "quantity" do
+	  it "cannot be nil" do
+	    product = FactoryGirl.build(:product, quantity: nil)
 
-  it "doesn't allow a product's quantity to be nil" do
-     macbook = FactoryGirl.build(:product, quantity: nil)
-     expect(macbook).to_not be_valid
-  end
+	    expect(product).to_not be_valid
+	  end
 
-  it "doesn't allow a product's quantity to be negative" do
-     macbook = FactoryGirl.build(:product, quantity: -1)
-     expect(macbook).to_not be_valid
-  end
+	  it "cannot be negative" do
+	    product = FactoryGirl.build(:product, quantity: -1)
 
+	    expect(product).to_not be_valid
+	  end
+	end
 end
+
+
