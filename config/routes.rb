e6676d7e5b47958c269a3f645dfd68e9fda43400
Brandy Burdick <blburdic@asu.edu>
2016-08-24 10:26:13 -0700

    Condense routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   
-  post 'products/create'
-
-  get 'products/new'
-
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/edit'
+  resources :products 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
