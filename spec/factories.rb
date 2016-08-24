815f616f115114ebbea7e01e867b7884f85b2bac
Brandy Burdick <blburdic@asu.edu>
2016-08-24 09:10:26 -0700

    Delete products.rb file from spec/factories and move content to new factories.rb file

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :product do
     name "MyString"
     quantity 1
-    price "9.99"
-    description "MyText"
+    price 9.99
+    description "Description"
+    promoted false
   end
 end
