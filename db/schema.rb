e55d42bda6d9aa7b7d0d4ea063fbb452843389a8
Brandy Burdick <blburdic@asu.edu>
2016-08-24 08:58:19 -0700

    Add promote column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823190532) do
+ActiveRecord::Schema.define(version: 20160824155648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160823190532) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "promoted"
   end
 
 end
