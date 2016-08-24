d30546a3939fe5facbfcbc2002de725db2610554
Brandy Burdick <blburdic@asu.edu>
2016-08-24 09:18:21 -0700

    Add spec to make sure description of product is not nill

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
 	validates :name, presence: true
 	validates :quantity, numericality: {greater_than_or_equal_to: 0}
 	validates_format_of :price, with: /\A\d+\.?\d{0,2}\z/
+	validates :description, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,6 +24,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+	
 	describe "name" do
 	  it "cannot be nil" do
 	    product = FactoryGirl.build(:product, name: nil)
@@ -33,13 +34,11 @@ RSpec.describe Product, type: :model do
 	describe "quantity" do
 	  it "cannot be nil" do
 	    product = FactoryGirl.build(:product, quantity: nil)
-
 	    expect(product).to_not be_valid
 	  end
 
 	  it "cannot be negative" do
 	    product = FactoryGirl.build(:product, quantity: -1)
-
 	    expect(product).to_not be_valid
 		end
 	end
@@ -47,10 +46,17 @@ RSpec.describe Product, type: :model do
 	describe "price" do
 	  it "cannot have 3 or more numbers after the decimal point" do
 	    product = FactoryGirl.build(:product, price: 78.456)
-
 	    expect(product).to_not be_valid
 	  end
 	end
+
+	describe "description" do
+    it "cannot be nil" do
+      product = FactoryGirl.build_stubbed(:product, description: nil)
+      expect(product).to_not be_valid
+    end
+  end
+  
 end
 
 
