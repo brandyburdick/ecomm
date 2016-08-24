fabb9e31fdc91a5e141e89253dc56ab819265988
Brandy Burdick <blburdic@asu.edu>
2016-08-24 09:32:04 -0700

    Add spec for .promoted method and write code to make it pass

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ActiveRecord::Base
 	validates :quantity, numericality: {greater_than_or_equal_to: 0}
 	validates_format_of :price, with: /\A\d+\.?\d{0,2}\z/
 	validates :description, presence: true
+	
+	def self.promoted
+	    where(promoted: true)
+	end
 end
