3308000b86499eea56ec8ca299b023b9267cf4ff
Brandy Burdick <blburdic@asu.edu>
2016-08-29 09:45:05 -0700

    Create spec for index view on Welcome controller

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 	let(:promoted_products) {FactoryGirl.build_stubbed_list(:product, 3, promoted: true)}
+  let(:non_promoted_products) {FactoryGirl.build_stubbed_list(:product, 3, promoted: false)}
+
+  it "renders #index with the correct dom elements" do
+    assign(:products, promoted_products)
+    render
+    expect(rendered).to have_content("Welcome to Tech Talent Store!")
+  end
+
+  it "renders #index with the correct products" do
+    assign(:products, promoted_products)
+    render
+
+    promoted_products.each do |product|
+      expect(rendered).to have_content(product.name)
+      expect(rendered).to have_selector("#myModal_#{product.id}")
+    end
+  end
 end
