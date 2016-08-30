7a88dd94c239dadeb30126fa732a02bceb08c7a8
Brandy Burdick <blburdic@asu.edu>
2016-08-30 12:08:49 -0700

    Add category column to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830190425) do
+ActiveRecord::Schema.define(version: 20160830190750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,10 @@ ActiveRecord::Schema.define(version: 20160830190425) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "promoted"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -74,4 +77,5 @@ ActiveRecord::Schema.define(version: 20160830190425) do
   add_foreign_key "carts", "users"
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "products"
+  add_foreign_key "products", "categories"
 end
