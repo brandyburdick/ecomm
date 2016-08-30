a52729c74426549f2aa804b8a4a27fed56622251
Brandy Burdick <blburdic@asu.edu>
2016-08-30 09:46:52 -0700

    Add products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+products = Product.create([
+	{name: "Macbook", price: 1000.24, description: "Great Buy", promoted: true, quantity: 24},
+	{name: "Chromebook", price: 800.99, description: "Amazing Value", promoted: true, quantity: 11}
+	])
+
