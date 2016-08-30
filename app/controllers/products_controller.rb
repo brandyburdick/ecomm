2b5a2e2989ba1a5e34cf0fe214f9b581d094553c
Brandy Burdick <blburdic@asu.edu>
2016-08-30 09:34:56 -0700

    Add name field to devise registration views and allow name to be saved to database through application controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
+  include CurrentCart
+  before_action :find_cart, only: [:line_item_create]
   before_action :authenticate_user!, except: :show
-  before_action :check_for_admin, except: :show
+  before_action :check_for_admin, except: [:show, :line_item_create]
 
   def new
   	@product = Product.new
@@ -42,7 +44,22 @@ class ProductsController < ApplicationController
   	redirect_to products_path
   end
 
+  def line_item_create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(products: product)
+    if @line_item.save
+      redirect_to cart_path(@line_item.cart)
+    else
+      redirect_to @product
+    end
+  end
+
   private
+    
+    def line_item_params
+      params.require(:line_item).permit(:cart_id, :product_id)
+    end
+
     def product_params
       params.require(:product).permit(:name, :quantity, :price, :description, :promoted)
     end
